fec0107d5e560f4ce4138512341b4aa6af634677
Serban Iordache <iordache@hotmail.com>
2016-02-29 08:34:59 +0100

    prepare development 0.3.0

--- projects/core/src/main/java/org/beryx/viewreka/core/VersionInfo.java
+++ projects/core/src/main/java/org/beryx/viewreka/core/VersionInfo.java
@@ -18,10 +18,10 @@ package org.beryx.viewreka.core;
 public class VersionInfo {
     // Do not edit the values below. They are configured by gradle before the execution of the compile task, based on the values found in gradle.properties.
     private static final int MAJOR = 0;
-    private static final int MINOR = 2;
+    private static final int MINOR = 3;
     private static final int PATCH = 0;
     private static final String LABEL = "";
-    private static final boolean RELEASE_BUILD = true;
+    private static final boolean RELEASE_BUILD = false;
 
     public static Version get() {
         return new Version(MAJOR, MINOR, PATCH, LABEL, RELEASE_BUILD);
